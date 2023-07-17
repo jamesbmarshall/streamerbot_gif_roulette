53c00eee2b34f6a9291b69394d3e2232dc22d950
James Marshall <jamesbmarshall@users.noreply.github.com>
2023-07-17 11:43:38 +0100

    Create gif_roulette.cs

---
+++ gif_roulette.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+public class CPHInline
+{
+	public bool Execute()
+	{
+
+  // Get the search term from the stream, such as from a channel point redeem or chat command.
+	string searchTerm = args["rawInput"].ToString();
+	
+	using (var webClient = new WebClient())
+            {
+                // You'll need to get your own API key for GIPHY to use in the URL below. This will return a GIF using the random endpoint, of a PG13 rating. You can adjust as you require, see the
+                // GIPHY docs for more details.
+                var jsonStr = webClient.DownloadString($"https://api.giphy.com/v1/gifs/random?api_key=YOUR_API_KEY&tag={searchTerm}&rating=pg13");
+
+                // Parse the JSON response.
+                var json = JObject.Parse(jsonStr);
+
+                // Extract the URL property.
+                var url = (string)json["data"]["embed_url"];
+
+                // Set a variable that you can use elsewhere in your action, such as to set the OBS browser source URL.
+                CPH.SetGlobalVar("gifurl", url, false);
+            }
+		return true;
+	}
+}
